bffa5a682853a2d30bcdac086e142fae38f20224
maltsevoff <alexmalczev@gmail.com>
2025-11-12 13:14:15 +0200

    init

---
+++ TypewriterText/TypewriterTextApp.swift
@@ -0,0 +1,17 @@
+//
+//  TypewriterTextApp.swift
+//  TypewriterText
+//
+//  Created by Aleksandr Maltsev on 12.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct TypewriterTextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
